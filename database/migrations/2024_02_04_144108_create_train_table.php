5517c01f755d36b884e89b46c7d240a55f5ceb1e
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-02-04 15:59:38 +0100

    creata tabella con relative colonne

--- database/migrations/2024_02_04_144108_create_train_table.php
+++ database/migrations/2024_02_04_144108_create_train_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('train', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 50);
+            $table->string('departure_station', 80);
+            $table->string('arrival_station', 80);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code'); // codice treno 
+            $table->integer('carriages_number'); // numero carrozze
+            $table->boolean('on_time')->default(true); // di default in orario 
+            $table->boolean('canceled')->default(false); // di default non cancellato
             $table->timestamps();
         });
     }
