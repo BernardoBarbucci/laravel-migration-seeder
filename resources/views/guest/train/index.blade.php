9c294fde77b1e8818e83f8f1d6707cb5940fcbe4
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-02-05 13:12:38 +0100

    modificate le cards mostrate con /train -> esercizio completato

--- resources/views/guest/train/index.blade.php
+++ resources/views/guest/train/index.blade.php
@@ -1,35 +1,46 @@
-@forelse ($trains as $train)
-    {{-- Card per ogni treno --}}
-    <div class="card m-2" style="width: 18rem;">
-        <div class="card-header">
-            {{ $train->train_code }} - {{ $train->company }}
+@extends('layout.app')
+
+@section('main-content')
+    <section class="container-fluid bg-dark">
+        <div class="d-flex flex-wrap justify-content-center">
+            @forelse ($trains as $train)
+            {{-- Card per ogni treno --}}
+            <div class="card m-2 bg-danger" style="width: 18rem;">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item bg-danger">
+                        <strong>Train code:</strong> {{ $train->train_code }}
+                    </li>
+                    <li class="list-group-item bg-light">
+                        <strong>Train code:</strong> {{ $train->train_code }}
+                    </li>
+                    <li class="list-group-item bg-light">
+                        <strong>Train company:</strong> {{ $train->company }}
+                    </li>
+                    <li class="list-group-item bg-light">
+                        <strong>Arrival Station:</strong> {{ $train->arrival_station }}
+                    </li>
+                    <li class="list-group-item bg-light">
+                        <strong>Departure Time:</strong> {{ $train->departure_time }}
+                    </li>
+                    <li class="list-group-item bg-light">
+                        <strong>Arrival Time:</strong> {{ $train->arrival_time }}
+                    </li>
+                    <li class="list-group-item bg-light">
+                        <strong>Carriages Number:</strong> {{ $train->carriages_number }}
+                    </li>
+                    <li class="list-group-item bg-light">
+                        <strong>On Time:</strong> {{ $train->on_time ? 'Yes' : 'No' }}
+                    </li>
+                    <li class="list-group-item bg-light">
+                        <strong>Canceled:</strong> {{ $train->canceled ? 'Yes' : 'No' }}
+                    </li>
+                </ul>
+            </div>
+            @empty
+                <div class="alert alert-info m-2" role="alert">
+                    No trains found in the database.
+                </div>
+            @endforelse
         </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">
-                <strong>Departure Station:</strong> {{ $train->departure_station }}
-            </li>
-            <li class="list-group-item">
-                <strong>Arrival Station:</strong> {{ $train->arrival_station }}
-            </li>
-            <li class="list-group-item">
-                <strong>Departure Time:</strong> {{ $train->departure_time }}
-            </li>
-            <li class="list-group-item">
-                <strong>Arrival Time:</strong> {{ $train->arrival_time }}
-            </li>
-            <li class="list-group-item">
-                <strong>Carriages Number:</strong> {{ $train->carriages_number }}
-            </li>
-            <li class="list-group-item">
-                <strong>On Time:</strong> {{ $train->on_time ? 'Yes' : 'No' }}
-            </li>
-            <li class="list-group-item">
-                <strong>Canceled:</strong> {{ $train->canceled ? 'Yes' : 'No' }}
-            </li>
-        </ul>
-    </div>
-@empty
-    <div class="alert alert-info m-2" role="alert">
-        No trains found in the database.
-    </div>
-@endforelse
+    </section>
+@endsection
