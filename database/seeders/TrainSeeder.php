ba8e4175bfd0eb74e25a5b19df94f80fc85c55f0
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-02-05 12:59:24 +0100

    tabella train seeded con successo attraverso faker

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -12,5 +12,20 @@ class TrainSeeder extends Seeder
 {
     public function run(Faker $faker): void
     {
+
+
+        for ($i = 0; $i < 100; $i++) {
+            $newTrain = new Train();
+            $newTrain->company = $faker->company();
+            $newTrain->departure_station = $faker->city();
+            $newTrain->arrival_station = $faker->city();
+            $newTrain->departure_time = $faker->time();
+            $newTrain->arrival_time = $faker->time();
+            $newTrain->train_code =  $faker->regexify('[A-Z]{10}[0-9]{8}');
+            $newTrain->carriages_number = $faker->numberBetween(5, 15);
+            $newTrain->on_time = $faker->boolean(30);
+            $newTrain->canceled = $faker->boolean(15);
+            $newTrain->save();
+        }
     }
 }
